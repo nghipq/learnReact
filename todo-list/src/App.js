20aec88a22aebe48c529012168dfc51cbc0c56fc
nghipq <nghipqce140179@fpt.edu.vn>
2019-05-25 21:02:18 +0700

    use Array.prototype

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Todoitem from './compornents/Todoitem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Todoitem title="bye meat" />
-        <Todoitem title="bye fish"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); 
+    this.todoitem = [
+      "bye meat",
+      "bye fish"
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoitem.map((item, index) => <Todoitem key={index} title= {item} />)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
