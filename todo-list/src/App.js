e4c9101625128449669246b428a7f70cfd08cb51
nghipq <nghipqce140179@fpt.edu.vn>
2019-05-28 21:31:26 +0700

    finish todo app

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
-import Todoitem from './compornents/Todoitem'
+import Todoitem from './compornents/Todoitem';
+import tick from './svg/tick.svg';
 
 class App extends Component {
   constructor() {
     super(); 
     this.state = {
-      todoitem: [
-      {title: "bye meat", isDone: true},
-      {title: "bye fish", isDone: false},
-    ]}
+      newItem: "",
+      todoitem: []
+    }
   }
 
   onItemClick(item) {
+    let { todoitem } = this.state;
+    let index = todoitem.indexOf(item);
     this.setState(
-      this.change(item)
+    {  todoitem: [
+        ...todoitem.slice(0, index),
+        {
+          ...item, isDone: !item.isDone
+        },
+        ...todoitem.slice(index + 1),
+      ]}    
     )
   }
 
-  change(item) {
-    return this.state.todoitem.filter(x => x === item).map(x => x.isDone = !item.isDone);
-  } 
+  onKeyUp(event) {
+    let text = event.target.value;
+    if(event.keyCode !== 13) {
+      return
+    }
+
+    text = text.trim();
+    if(!text) {
+      return
+    }
+
+    this.setState({
+      newItem: "",
+      todoitem: [
+        {title: text, isDone: false},
+        ...this.state.todoitem
+      ]
+    })
+  }
+
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    })
+  }
 
   render() {
-  const { todoitem } = this.state
+  const { todoitem, newItem } = this.state
     return (
       <div className="App">
+        <div className="InputPlace">
+          <img src={tick} width={32} height={32} alt="..." />
+          <input type="text"
+           placeholder="ADD NEW TODO"
+           value={newItem}
+           onChange={this.onChange.bind(this)}
+           onKeyUp={this.onKeyUp.bind(this)} />
+        </div>
         {
-        todoitem.length > 0 && todoitem.map((item, index) => <Todoitem onClick={this.onItemClick.bind(this, item)} key={index} item= {item} />)
+        todoitem.length > 0 && todoitem.map((item, index) => 
+        <Todoitem 
+        onClick={this.onItemClick.bind(this, item)} 
+        key={index} 
+        item= {item} />)
         }
         {
         todoitem.length === 0 && 'notthing here!'
--- todo-list/src/compornents/Todoitem.js
+++ todo-list/src/compornents/Todoitem.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './Todoitem.css';
+import check from '../svg/success.svg';
+import checkDone from '../svg/success-done.svg';
 
 const classNames = require('classnames');
 
 class Todoitem extends Component {
     render() {
         const {item, onClick} = this.props;
+        let url = check;
+        if(item.isDone) {
+            url = checkDone
+        }
         let className = classNames('Todoitem', {TodoitemDone: item.isDone});
 
         return (
-            <div onClick={onClick} className={className}>
-                {item.title}
+            <div className={className}>
+                <img onClick={onClick} src={url} alt="..."/>
+                <p>{item.title}</p>
             </div>
         );
     };
